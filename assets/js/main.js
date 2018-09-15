735a15515127fb5e3f2423c24029e2caaa956b35
Kathleen Doviken <kathleendoviken@gmail.com>
2018-09-15 15:39:03 -0500

    sidebars...working???

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
+let sidebar = $('#sidebar-wrapper');
 $('.navbar-toggler').click(function () {
     event.preventDefault();
-    let sidebar = $('#sidebar-wrapper');
     if (sidebar.hasClass('sidebar-out')) {
         console.log("this")
         // slide in
@@ -22,3 +22,31 @@ $('.navbar-toggler').click(function () {
     }
     console.log($(".page").css("left") == "0px");
 });
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        // slide in
+        sidebar.css({
+            "left": "0px",
+            "width": "250px",
+            "transition": "all 0.4s ease 0s",
+        });
+        $('#wrapper').css({"padding": "0"});
+        if (sidebar.hasClass('sidebar-out')) {
+            $('#sidebar-wrapper').removeClass("sidebar-out");
+        }
+    } else {
+        // slide out
+        $('#wrapper').css({ "padding-left": "250px" })
+        sidebar.css({
+            "left": "250px",
+            "width": "250px",
+            "transition": "all 0.4s ease 0s",
+        });
+        $('#sidebar-wrapper').addClass("sidebar-out");
+    }
+}
+
+let x = window.matchMedia("(max-width: 767px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
