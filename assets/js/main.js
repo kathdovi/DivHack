92b86d6fa635f4c29e39790b85c871e48bb69259
Kathleen Doviken <kathleendoviken@gmail.com>
2018-09-15 15:18:54 -0500

    nav bars but it's kinda broken

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
-$('.menubutton').toggle(function () {
-    $('#A').animate({ width: 0 });
-    $('#B').animate({ left: 0 });
-}, function () {
-    $('#A').animate({ width: 200 });
-    $('#B').animate({ left: 200 });
-})
+$('.navbar-toggler').click(function () {
+    event.preventDefault();
+    let sidebar = $('#sidebar-wrapper');
+    if (sidebar.hasClass('sidebar-out')) {
+        console.log("this")
+        // slide in
+        sidebar.css({
+            "left": "0px",
+            "width": "250px",
+            "transition": "all 0.4s ease 0s",
+        });
+        $('#sidebar-wrapper').removeClass("sidebar-out");
+    } else {
+        console.log("that")
+        // slide out
+        sidebar.css({
+            "left": "250px",
+            "width": "250px",
+            "transition": "all 0.4s ease 0s",
+        });
+        $('#sidebar-wrapper').addClass("sidebar-out");
+    }
+    console.log($(".page").css("left") == "0px");
+});
