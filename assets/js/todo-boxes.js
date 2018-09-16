f79d7a9cb7a89415c801eb20a2ee3e7b816b15b3
niholm <nicole@blacklocus.com>
2018-09-16 12:58:25 -0500

    Add todo support

--- assets/js/todo-boxes.js
+++ assets/js/todo-boxes.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
       var obj = jQuery.parseJSON( data.body );
 
       $.each(obj, function(i, item) {
-
+        
     		console.log(item.EventTitle);
+        var row = '<h3 class="subhead"><a href="checklist.html">Plan '+item.EventTitle+'</a></h3><ul class="event-info">'
+        $('#show-todo-boxes').append(row);
         for (key in item) {
-          console.log('oh hi');
-          console.log(key);
           if (item[key] === false) {
               console.log('need to do this');
-              var row = '<label><input type="checkbox"> '+item.EventTitle+': '+ key+'</label><br><br>'
-              $('#show-data').append(row);
+              $('#show-todo-boxes').append('<li>'+key+'</li>');
           }
         }
+        $('#show-todo-boxes').append('</ul><br>');
 	  });
     });
 });
