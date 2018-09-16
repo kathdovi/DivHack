4d92a58047e64e4afbbc28c1aafdff8997c30197
niholm <nicole@blacklocus.com>
2018-09-15 22:07:03 -0500

    Fix event handling

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,15 +1,28 @@
-(function eventWrapper($) {
-
-    function submitform(){
-        var formData = JSON.stringify($("#event-form").serializeArray());
-        alert(formData);
+$(document).ready(function(){
+        $("#formid").on('submit', function(){
+            var title = $("#event-title").val();
+            var date = $("#date").val();
+            var time = $("#time").val();
+            var location = $("#location").val();
         $.ajax({
-          type: "POST",
-          url: _config.api.invokeUrl + '/event',
-          data: formData,
-          success: function(){},
-          dataType: "json",
-          contentType : "application/json"
-        });
-
-}(jQuery)});
+            method: 'POST',
+            url: 'https://bllvi5u5fc.execute-api.us-east-1.amazonaws.com/prod/event',
+            data: JSON.stringify(
+                {'EventTitle': title,
+                 'Date': date,
+                 'Time': time,
+                 'Location': location}),
+            type: 'post',
+            contentType: 'application/json',
+            success: function completeRequest(result) {
+                console.log('we good');
+            },
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error creating event: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when creating event:\n' + jqXHR.responseText);
+            }
+       });
+    return false;
+    });
+});
